c7825659f6805a9859af3b63311217bcdfbbee99
Matthew Shenar <matthew.shenar@gmail.com>
2023-06-23 11:47:52 +0200

    working routes

--- src/API.jsx
+++ src/API.jsx
@@ -1,13 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import SearchBar from './SearchBar.jsx';
 import SearchResult from './SearchResult.jsx';
 import { acessKey } from './keys.js';
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function API() {
 
     const [imageSearch, setImageSearch] = useState('');
 
     const [searchTerm, setSearchTerm] = useState('');
+
+    const {term} = useParams();
 
     const url = `https://api.unsplash.com/search/photos?per_page=12&query=${searchTerm}&client_id=${acessKey}`
 
@@ -27,8 +31,12 @@ export default function API() {
 
     return (
         <>
-            <SearchBar setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
-            <SearchResult imageSearch={imageSearch} />
+        <Routes>
+
+            <Route path="/" element= {<SearchBar setSearchTerm={setSearchTerm} />} />
+            <Route path="/search" element= {<SearchBar setSearchTerm={setSearchTerm} />} />
+            <Route path='/search/:term' element={<SearchResult imageSearch={imageSearch} />} />
+        </Routes>
         </>
     )
 
